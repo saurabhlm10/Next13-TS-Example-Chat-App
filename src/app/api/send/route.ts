dfbafd8850fe7b52bed0c5663ba1e60ac538424f
saurabhlm10 <saurabhlm10@gmail.com>
2023-06-01 11:49:24 +0530

    minor bug fix

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -2,9 +2,17 @@ import { pusherServer } from "@/lib/pusher";
 
 export async function POST(req: Request) {
   try {
+    console.log('POST1')
+    
     const { text } = await req.json();
 
+    console.log('POST2')
+
+    console.log('TEXT',text)
+
     pusherServer.trigger("chat", "incoming-message", text);
+
+    console.log('POST3')
 
     return new Response("OK");
   } catch (error) {
