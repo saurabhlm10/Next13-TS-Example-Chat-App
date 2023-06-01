9e3ecdb6e17fe10a023e64d6cf4c6479f9555e46
saurabhlm10 <saurabhlm10@gmail.com>
2023-06-01 11:58:38 +0530

    minor bug fix

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -10,7 +10,7 @@ export async function POST(req: Request) {
 
     console.log('TEXT',text)
 
-    pusherServer.trigger("chat", "incoming-message", text);
+    pusherServer.trigger("chat", "incoming-message", {text});
 
     console.log('POST3')
 
